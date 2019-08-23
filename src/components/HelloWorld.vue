94bbd53a5b09753cb8457a8ea9bc7fff5b2cebc8
chenzf <372624240@qq.com>
2019-08-23 18:10:05 +0800

    1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,23 +1,52 @@
 <template>
   <div>
     <img id="barcode"/>
+    <div id='imgBox'></div>
+    <div @click='handleCreate'>批量生成条形码</div>
   </div>
 </template>
 
 <script>
-import barCode from 'jsbarcode'
+// import barCode from 'jsbarcode'
 
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      numData: {
-        ean13: 1234567890128
-      }
+      numData: [
+        '2019082342773-1-0',
+        '2019082342773-1-1',
+        '2019082342773-1-2',
+        '2019082342773-1-3',
+        '2019082342773-1-4'
+      ]
     }
   },
   mounted () {
-    barCode('#barcode').options({font: 'OCR-B'}).EAN13(this.numData.ean13, {fontSize: 18, textMargin: 0}).blank(20).render()
+    // barCode('#barcode').options({font: 'OCR-B'}).CODE128B(this.numData.ean13, {fontSize: 18, textMargin: 0}).blank(20).render()
+  },
+  methods: {
+    handleCreate () {
+      // let _this = this
+      // let tempData = _this.numData
+      // for (let i in tempData) {
+      //   let image = new Image()
+      //   image.onload = () => {
+
+      //   }
+      //   // console.log(tempData[i])
+      // }
+      console.log(12)
+      let reader = new FileReader()
+      let image = new Image()
+      reader.onload = function () {
+        image.onload = function () {
+          console.log(123)
+          image.id = 'aa'
+          document.getElementById('imgBox').appendChild(image)
+        }
+      }
+    }
   }
 }
 </script>
